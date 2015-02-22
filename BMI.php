0c4fc59797455cca5d8a655189dd139ef8c656b6
fuurin <komatsu0624@docomo.ne/jp>
2015-02-22 15:56:14 +0900

    サンプル詰め合わせ

---
+++ BMI.php
@@ -0,0 +1,81 @@
+﻿<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Body Mass Index</title>
+<link rel="stylesheet" href="HTMLの復習.css" type="text/css">
+</head>
+<body>
+	<h1>Body Mass Index</h1>
+
+<form action="check6.php" method="post">
+	体重:<input type="text" name="weight" value=<?php echo $_POST["weight"] ?> />kg
+	<?php if( ( $_POST["weight"] >= 1.0 && $_POST["weight"] <= 200.0 ) == FALSE )
+		{
+			echo "<font color = 'red'> ← 1 ～ 200の数字(半角)を入力してください。</font>";
+		}
+	?>
+	<br/><br/>
+
+	身長:<input type="text" name="height"  value=<?php echo $_POST["height"] ?> />cm
+	<?php if( ( $_POST["height"] >= 1.0 && $_POST["height"] <= 200.0 ) == FALSE )
+		{
+			echo "<font color = 'red'> ← 1 ～ 200の数字(半角)を入力してください。</font>";
+		}
+	?>
+	<br/><br/>
+
+	<input type="submit" value="計算する" />
+	<br/><br/>
+</form>
+
+	BMI:
+	<?php
+		if( ($_POST["weight"] >= 1.0 && $_POST["weight"] <= 200.0 && $_POST["height"] >= 1.0 && $_POST["height"] <= 200.0) == FALSE )
+		{
+			echo "<font color = 'red'>ERROR</font>";
+		}
+		else if(isset($_POST["weight"], $_POST["height"]) == true)
+		{
+			$BMI = 10000 * $_POST["weight"] / ($_POST["height"] * $_POST["height"]);
+			printf(" %.1f", $BMI);
+		}
+	?>
+	<br/><br/>
+
+	判定:
+	<?php
+		if( ($_POST["weight"] >= 1.0 && $_POST["weight"] <= 200.0 && $_POST["height"] >= 1.0 && $_POST["height"] <= 200.0) ==FALSE )
+		{
+			echo "<font color = 'red'>ERROR</font>";
+		}
+		else if(isset($_POST["weight"], $_POST["height"]) == true)
+		{
+			$BMI = 10000 * $_POST["weight"] / ($_POST["height"] * $_POST["height"]);
+			
+			if( $BMI < 18.5 )echo "低体重";
+			elseif( $BMI < 25 )echo "標準";
+			elseif( $BMI < 30 )echo "肥満(1度)";
+			elseif( $BMI < 35 )echo "肥満(2度)";
+			elseif( $BMI < 40 )echo "肥満(3度)";
+			elseif( $BMI >= 40 )echo "肥満(4度)";
+		}
+	?>
+	<br/><br/>
+
+	<h3>参考</h3>
+
+	BMI = 体重[kg]/(身長[m]^2)</br></br>
+
+	日本肥満学会の肥満基準(2000年)</br>
+
+<table>
+	<tr><th>状態</th><th>指標</th></tr>
+	<tr><td>標準体重</td><td>18.5以上、25未満</td></tr>
+	<tr><td>肥満(1度)</td><td>25以上、30未満</td></tr>
+	<tr><td>肥満(2度)</td><td>30以上、35未満</td></tr>
+	<tr><td>肥満(3度)</td><td>35以上、40未満</td></tr>
+	<tr><td>肥満(4度)</td><td>40以上</td></tr>
+</table>
+
+</body>
+</html>
---
+++ fortune.php
@@ -0,0 +1,73 @@
+﻿<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>サンプル</title>
+</head>
+<body>
+
+<h2>瞬間運勢(漢字時計版)</h2>
+
+<p>リロードするたびにその瞬間の運勢が占えます。</p>
+
+<h1>
+<?php
+	function kanji( $time )
+	{
+		$t	=	(int)$time;
+		$fig 	=	(int)log10( $t );
+		$string	=	"";
+
+		if($t==0)return "零";
+
+		for($i=0;$i<=$fig;$i++)
+		{
+			if( $i==1 )$string .= "十";
+			
+			//文字列切り取り関数、$tの$i文字目から1文字切り取る
+			switch( substr($t, $i, 1) )
+			{
+				//一が付くのは最後の桁のときだけ
+				case 1:	if($i==$fig)$string .= "一";	break;
+				case 2:	$string .= "二";		break;
+				case 3:	$string .= "三";		break;
+				case 4:	$string .= "四";		break;
+				case 5:	$string .= "五";		break;
+				case 6:	$string .= "六";		break;
+				case 7:	$string .= "七";		break;
+				case 8:	$string .= "八";		break;
+				case 9:	$string .= "九";		break;
+				default:				break;
+			}
+		}
+		
+		return $string;
+	}
+
+	date_default_timezone_set("Asia/Tokyo");
+
+	if( date("a") == "am" )	echo "午前";
+	else 			echo "午後";
+
+	$g = kanji( date("g") );
+	$i = kanji( date("i") );
+	$s = kanji( date("s") );
+
+	echo "{$g}時{$i}分{$s}秒";
+?>
+
+<br/>
+現在の運勢は<?php
+
+	$fortune = array( "大吉", "中吉", "小吉", "吉", "末吉", "凶", "大凶" );
+
+	$count = count( $fortune );
+
+	//乱数発生関数。rand(最小値,最大値) 重い？
+	$random = rand( 0, $count-1 );
+
+	echo $fortune[ $random ];
+?>です。
+</h1>
+
+</body>
+</html>
